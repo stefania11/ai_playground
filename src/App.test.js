4860a2573e26fee709ded245ecb0db851ee48d7e
stefania11 <druga.stefania@gmail.com>
2022-02-22 15:18:33 -0800

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders playground', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/playground/i);
-  expect(linkElement).toBeInTheDocument();
-});
