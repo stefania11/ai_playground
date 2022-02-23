2e12aa7ede89f7595c7849a56404d995594d0081
stefania11 <druga.stefania@gmail.com>
2022-02-23 13:59:27 -0800

    Updates

--- precache-manifest.98f53bbe2a2a1c61982e8d7d16f2bca9.js
+++ precache-manifest.98f53bbe2a2a1c61982e8d7d16f2bca9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f251c29e9738cef9bd5b146824ebca04",
+    "revision": "d14d03fd4c1dad2715dee211034328da",
     "url": "/ai_playground/index.html"
   },
   {
-    "revision": "c8439fbf2521ea58d2af",
+    "revision": "8455150485ccd433c85c",
     "url": "/ai_playground/static/css/main.4160fa8d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ai_playground/static/js/3.fe89119d.chunk.js"
   },
   {
-    "revision": "c8439fbf2521ea58d2af",
-    "url": "/ai_playground/static/js/main.b326d54d.chunk.js"
+    "revision": "8455150485ccd433c85c",
+    "url": "/ai_playground/static/js/main.af1534ce.chunk.js"
   },
   {
     "revision": "483216ef086785af7508",
