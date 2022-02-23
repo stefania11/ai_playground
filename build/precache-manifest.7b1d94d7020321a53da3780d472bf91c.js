2f7064019c6b459ad8ff4df33ad64c123dc3f853
stefania11 <druga.stefania@gmail.com>
2022-02-22 18:14:47 -0800

    deploy

---
+++ build/precache-manifest.7b1d94d7020321a53da3780d472bf91c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04becbab18d2ddf62e2320eb549ce8cc",
+    "url": "/ai_playground/index.html"
+  },
+  {
+    "revision": "f4e7e7f8b0e1992ba215",
+    "url": "/ai_playground/static/css/main.44e09747.chunk.css"
+  },
+  {
+    "revision": "3f29ae3d6bfa0c5c5f52",
+    "url": "/ai_playground/static/js/2.749e0811.chunk.js"
+  },
+  {
+    "revision": "4c7a19e055998208c1c3",
+    "url": "/ai_playground/static/js/3.43ac7eac.chunk.js"
+  },
+  {
+    "revision": "f4e7e7f8b0e1992ba215",
+    "url": "/ai_playground/static/js/main.ee5655dd.chunk.js"
+  },
+  {
+    "revision": "65bde51d765bcb7c0cab",
+    "url": "/ai_playground/static/js/runtime~main.416fdd1e.js"
+  },
+  {
+    "revision": "d775206a069e26c49b515462db343e6c",
+    "url": "/ai_playground/static/media/actBackgroundB.d775206a.png"
+  },
+  {
+    "revision": "b0ded2f9956bf2d67e99b814cb5391dc",
+    "url": "/ai_playground/static/media/actBackgroundG.b0ded2f9.png"
+  },
+  {
+    "revision": "812c3c270b0ad79d02eeccf17e42fd92",
+    "url": "/ai_playground/static/media/actBackgroundP.812c3c27.png"
+  },
+  {
+    "revision": "0cca5017cf901f54d500e7f88134753e",
+    "url": "/ai_playground/static/media/actBackgroundY.0cca5017.png"
+  },
+  {
+    "revision": "3fca610283f97edba1a7e033e1a5113e",
+    "url": "/ai_playground/static/media/hpBackground.3fca6102.png"
+  },
+  {
+    "revision": "f18203d1a79aa833d88570fb94d2ff2a",
+    "url": "/ai_playground/static/media/innerActBackgroundB.f18203d1.png"
+  },
+  {
+    "revision": "f72645181072dd98f4da622e3a471c2b",
+    "url": "/ai_playground/static/media/innerActBackgroundG.f7264518.png"
+  },
+  {
+    "revision": "c6360091039594f9b9d53b37f4f74b09",
+    "url": "/ai_playground/static/media/innerActBackgroundP.c6360091.png"
+  },
+  {
+    "revision": "7abb981a812c797c6e72d74aadc52232",
+    "url": "/ai_playground/static/media/innerActBackgroundY.7abb981a.png"
+  }
+]);
